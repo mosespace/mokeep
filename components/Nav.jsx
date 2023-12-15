5f3a81c74476275e3911d093c7cb94454a1a27f1
mosespace <kisakyemoses50@gmail.com>
2023-12-15 15:51:43 +0300

    feat: initial commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -116,33 +116,18 @@ export const Nav = () => {
               <div className='absolute top-0 left-0 w-full'>
                 <div className='p-5 bg-white border rounded shadow-sm'>
                   <div className='flex items-center justify-between mb-4'>
-                    <div>
-                      <Link
-                        href='/'
-                        aria-label='Company'
-                        title='Company'
-                        className='inline-flex items-center'
-                      >
-                        <svg
-                          className='w-8 text-deep-purple-accent-400'
-                          viewBox='0 0 24 24'
-                          strokeLinejoin='round'
-                          strokeWidth='2'
-                          strokeLinecap='round'
-                          strokeMiterlimit='10'
-                          stroke='currentColor'
-                          fill='none'
-                        >
-                          <rect x='3' y='1' width='7' height='12' />
-                          <rect x='3' y='17' width='7' height='6' />
-                          <rect x='14' y='1' width='7' height='6' />
-                          <rect x='14' y='11' width='7' height='12' />
-                        </svg>
-                        <span className='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
-                          Company
-                        </span>
-                      </Link>
-                    </div>
+                    {/* <div>
+                      <Image
+                        width={96}
+                        height={96}
+                        src='/desishub-coding-school-logo.png'
+                        alt='desishub-coding-school-logo'
+                        className='w-[4.5rem] text-teal-accent-400'
+                      />
+                    </div> */}
+                    <span className='text-xl font-bold tracking-wide  text-red-600'>
+                      Mo<span className='font-light'>Keep</span>
+                    </span>
                     <div>
                       <button
                         aria-label='Close Menu'
@@ -161,37 +146,7 @@ export const Nav = () => {
                   </div>
                   <nav>
                     <ul className='space-y-4'>
-                      <li>
-                        <Link
-                          href='/'
-                          aria-label='Our product'
-                          title='Our product'
-                          className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
-                        >
-                          Product
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          href='/'
-                          aria-label='Our product'
-                          title='Our product'
-                          className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
-                        >
-                          Features
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          href='/'
-                          aria-label='Product pricing'
-                          title='Product pricing'
-                          className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
-                        >
-                          Pricing
-                        </Link>
-                      </li>
-                      <li>
+                      {/* <li>
                         <Link
                           href='/'
                           aria-label='About us'
@@ -200,11 +155,11 @@ export const Nav = () => {
                         >
                           About us
                         </Link>
-                      </li>
+                      </li> */}
                       <li>
                         <Link
-                          href='/'
-                          className='inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none'
+                          href='/register'
+                          className='inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-red-600 hover:bg-red-700 focus:shadow-outline focus:outline-none'
                           aria-label='Sign up'
                           title='Sign up'
                         >
