cef6c4685618af15fffc0db985a6b87ee04d44ba
mosespace <kisakyemoses50@gmail.com>
2024-02-09 15:06:39 +0300

    feta: updated category

--- components/admin/SubCategory.jsx
+++ components/admin/SubCategory.jsx
@@ -25,7 +25,6 @@ export default function SubCategory({ data }) {
                 <Link
                   href={`/admin/subCategory/${item.id}`}
                   className='align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-6 rounded-lg bg-green-600 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none'
-                  type='button'
                 >
                   Edit
                 </Link>
