be57fdeae9eefa8e7cc1c6ba6f4c66091119811f
kooza-colline <koozacollinz1@gmail.com>
2024-02-09 14:19:11 +0300

    feat: collinz feature

--- app/layout.js
+++ app/layout.js
@@ -17,8 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        {children}
-        {/* <AuthProvider>
+        <AuthProvider>
           <div className='flex lg:flex-row flex-col w-full'>
             <div className='md:w-64 md:block hidden overflow-hidden'>
               <SideBar />
@@ -32,7 +31,7 @@ export default function RootLayout({ children }) {
             </div>
           </div>
         </AuthProvider>
-        <Toaster position='top-center' expand={false} richColors closeButton /> */}
+        <Toaster position='top-center' expand={false} richColors closeButton />
       </body>
     </html>
   );
